0e21d3c860a7618c6f9b3c465901e4648fdd23f6
minwoo <minwoo.yim@gmail.com>
2020-04-15 09:46:47 +1000

    Progress in unicafe

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,17 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Header = (props) => {
+	console.log('Header props is', props);
+	return (
+		<div>
+			<h1>{props.headerText}</h1>
+		</div>
+	);
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Statistics = (props) => {
+	console.log('statisticsData prop is', props);
+	console.log('props.good is', props.statisticsData.good);
+
+	//conditional rendering used here
+	if (props.statisticsData.all === 0) {
+		return (
+			<div>
+				<h1>{props.statisticsData.headerText}</h1>
+				<p> No feedback given </p>
+			</div>
+		);
+	}
+
+	return (
+		<div>
+			<h1>{props.statisticsData.headerText}</h1>
+			<p>good {props.statisticsData.good}</p>
+			<p>neutral {props.statisticsData.neutral}</p>
+			<p>bad {props.statisticsData.bad}</p>
+			<p>all {props.statisticsData.all}</p>
+			<p>average {props.statisticsData.average}</p>
+			<p>positive {props.statisticsData.positive}</p>
+		</div>
+	);
+};
+
+const Button = (props) => {
+	return (
+    <button onClick={props.handleClick}>{props.text}</button>
+	);
+};
+
+const App = () => {
+	// save clicks of each button to own state
+	const [ good, setGood ] = useState(0);
+	const [ neutral, setNeutral ] = useState(0);
+	const [ bad, setBad ] = useState(0);
+	const [ all, setAll ] = useState(0);
+
+	const setGoodValue = () => {
+		setGood(good + 1);
+		setAll(all + 1);
+		console.log('Good is', good);
+		console.log('All is', all);
+	};
+
+	const setNeutralValue = () => {
+		setNeutral(neutral + 1);
+		setAll(all + 1);
+	};
+
+	const setBadValue = () => {
+		setBad(bad + 1);
+		setAll(all + 1);
+	};
+
+	const statisticsData = {
+		headerText: 'statistics',
+		good: good,
+		neutral: neutral,
+		bad: bad,
+		all: good + neutral + bad,
+		average: (good * 1 + neutral * 0 + bad * -1) / all,
+		positive: good / all
+  };
+
+	const topHeaderText = 'give feedback';
+
+	return (
+		<div>
+			<Header headerText={topHeaderText} />
+			<Button handleClick={() => setGoodValue(good+1)} text="good"/>
+      <Button handleClick={() => setNeutralValue(good+1)} text="neutral"/>
+      <Button handleClick={() => setBadValue(good+1)} text="bad"/>
+			<Statistics statisticsData={statisticsData}/>
+		</div>
+	);
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
